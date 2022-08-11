ebe9168d551cd44ca715e61e19332bc5990e2f86
GabrielTomonariDev <gabriel@tomonari.dev>
2022-08-11 20:39:54 -0300

    Fix doughnut chart

--- src/components/dashboard/doughnutChart.tsx
+++ src/components/dashboard/doughnutChart.tsx
@@ -1,6 +1,9 @@
 import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
-import { transactionsPerPaymentPointer, transactionsResults } from "../../lib/database/databaseService";
+import {
+  transactionsPerPaymentPointer,
+  transactionsResults,
+} from "../../lib/database/databaseService";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -10,8 +13,8 @@ const options = {
 };
 
 const DoughnutChart = (props: { revenueStatistics: transactionsResults }) => {
-
-  {/*
+  {
+    /*
 
   -> Fazer um loop em cada objeto do array monthData
 
@@ -38,40 +41,42 @@ const DoughnutChart = (props: { revenueStatistics: transactionsResults }) => {
     randomColor = Math.floor(Math.random() * 16777215).toString(16);
   }
 
-*/}
-
-  let chartData: number[][] = []
-  let labels: string[] = []
-  
-  for (let i=0; i<props.revenueStatistics.monthData.length; i++) {
-    if (!chartData[i]){
-      chartData[i] = []
-    }
-    let totalSum = 0
-    for(let j=0; j<12; j++) {
-      totalSum += props.revenueStatistics.monthData[i][j] ?? 0
-    }
-    chartData[i].push(totalSum)
-    labels.push(props.revenueStatistics.monthData[i].paymentPointer.slice(0, -5))
+*/
   }
 
+  let chartData: number[][] = [];
+  let labels: string[] = [];
+
+  for (let i = 0; i < props.revenueStatistics.monthData.length; i++) {
+    if (!chartData[i]) {
+      chartData[i] = [];
+    }
+    let totalSum = 0;
+    for (let j = 0; j < 12; j++) {
+      totalSum += props.revenueStatistics.monthData[i][j] ?? 0;
+    }
+    chartData[i].push(totalSum);
+    labels.push(
+      props.revenueStatistics.monthData[i].paymentPointer.slice(0, -5)
+    );
+  }
 
   const data = {
     labels: labels,
-    datasets: 
-      chartData.map((data) => {
-        let randomColor = "fffff";
-        while (randomColor.length == 5) {
-          randomColor = Math.floor(Math.random() * 16777215).toString(16);
-          randomColor = "#" + randomColor;
-        }
-        return(
-        {
-          data: data,
-          backgroundColor: [randomColor, randomColor],
-          borderWidth: 1,
-        }
-      )})
+    datasets: [
+      {
+        data: chartData.map((x) => x[0]),
+        backgroundColor: [
+          "#FFA02E",
+          "#CC6F00",
+          "#FF8B00",
+          "#663800",
+          "#995300",
+          "#331C00",
+        ],
+        borderWidth: 1,
+      },
+    ],
   };
 
   return <Doughnut data={data} options={options} />;
